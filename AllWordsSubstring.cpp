9ee597278bbd3f74cb6bd41c5c5c7cbedf16f86c
Sergey <mr.ultimateground@gmail.com>
2024-12-26 12:12:18 +0100

    Add project files.

---
+++ AllWordsSubstring.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<catch.hpp>
+#include<vector>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+bool DoesContentAnyWords(string::iterator str_iter,size_t str_size,vector<string>& words) {
+	bool any_words_left = false;
+
+	auto res = find_if(words.begin(), words.end(), [&any_words_left,str_iter,str_size](string s) {
+		if (s.empty())
+			return false;
+		any_words_left = true;
+		if (s.size() > str_size) 
+			return false;
+		if (*str_iter != s.front())
+			return false;
+		return equal(s.begin(), s.end(), str_iter);
+		});
+
+	if (!any_words_left)
+		return true;
+	if (str_size == 0)
+		return false;
+
+	if (res == words.end())
+		return false;
+	else {
+
+		str_size -= (*res).size();
+		str_iter += (*res).size();
+		*res = "";
+		
+		return DoesContentAnyWords(str_iter, str_size, words);
+
+	}
+
+	return false;
+}
+
+vector<int> findSubstring(string s, vector<string> words) {
+	vector<int> result{};
+	auto str_iter = s.begin();
+	for (size_t i = 0; i < s.size(); i++)
+	{
+		vector<string> temp(words);
+		if (DoesContentAnyWords(str_iter,s.size()-i,temp))
+		{
+			result.push_back(i);
+		}
+
+		str_iter++;
+	}
+
+	return result;
+}
+
+TEST_CASE("BASE TEST") {
+	REQUIRE(findSubstring("barfoothefoobarman"s, vector<string>{"foo", "bar"}) == vector<int>{0, 9});
+	REQUIRE(findSubstring("wordgoodgoodgoodbestword"s, vector<string>{"word", "good", "best", "word"}) == vector<int>{});
+	REQUIRE(findSubstring("barfoofoobarthefoobarman"s, vector<string>{"bar", "foo", "the"}) == vector<int>{6, 9, 12});
+}
+
+TEST_CASE("SPEED TEST") {
+	BENCHMARK("1") {
+		findSubstring("aratdgpfsookqyxiptwjllwirwmfdmafdayignqadektejbvcaepnbyezmlcalkfnkovaeydkixqsalwaqmptmwsajwaxifktvitbgwlqvdiwphyhtochmwhvujsdofymptnvpzlnewjtfhcaxghjeqhzzzepgebvxpqoqebuckqhuuhtbqdfgmghlmwuqypppiefofbqsrzooihpymwgyfnmrlgkjxzaffftnjlfamvoucfurecpjpdcblomjqnovukidvpgikwepebgidxttdxwomkzczqxuaizxxhfkembinjsqglllijcwubytryptgrrfyypekqmxzeqirbvclxyhbxcuxuodlfilfaqqsepulfecjzgwzfdaehubbkeorocxqunjycflkjqtkwpjbsjfbggdqmtibvhtpucqyjbisrbfosopmaxogdryqmitkpmzlrrkatuvmyaaqosdqpryukxkwhfgzuiklzwnurkhpkjfbbirsbqnlpzkyfsxbcimeebyxmhmyruzsctlglytaxfyeyyfcgtgmxfkjowjfqjyveldplwricppcnwevsnkpuuaisjoluoiyzlnkjeulwlpliujahtrdntlhczsxncxonlyxrvkngabowkqffjiuctbhvweiybxmoxlisvvzxyptkiyprijwcndxjjhhmgvbeijwegvdhnxrvxjoiskmfnloysylzvmfexbifznmmmgjkpxujkgpzrfyjfjkbxpfwtpbscqgpwrbnovwadlautyfahqbehiccxdwctcrrvuekrbkuokojktkrlnxvwzxtfuvayosuiygzzstqfjzpfljszinvilqphopfejzpihqjamzfvbuamftloiiyvbncrekvolkytbhtnjakkulwagkqquomvbomhykvkuvdxdykoyvyuiwwqgmgdasydjnlhxitreyrjwbxwyxritnacahfcbdgksrupzpjfoaomdnjekamepngpwlsxqakjrgasvjyjlowpcftohhuwwaounisgiepbkpthbtrmipbmuwcdgxzczpxnxrthxkijbeqcxhphysvuryxzxzbwoalmtgvsqedsiuwhdlrcgmwzmuyjvbijetxzatsszvmjrjqvwhzeuvdirbumdcyhxvizqaspygyprrmoyluuhyfabzpgbnpykyadasuyavosmaimocjxcoxgnooqdeqqeqtekpflktfyvnllruiwigexwsnaydqcwljgaotljyjgnwshsafaijzfdmfcsvgogruifovqohxcdrjwroyazyfcmmgejpsrgnzodvercdtfuedoueotcvyqhgvsvnjeciuhfazheupabuovycrczptixoatdlziutpbtqjzlaamuojvtaxsrsdpidkmplulejmzcxznzbbupkrinqdgjgvrlhdcsznylcynbzcbadxppoaeedlplihwqmzkoojbqjouksvkvubmidtoiegtonqgbrrrfcsynisxanjxhkmrqfhylnszckigbbxgdqeqawpblyyukmlarvzcitelfmtgnqosivkjtzmfxmzjznkqilvsgzflnytcpjwzegkyfwfejgnivzadkvyswdflcyptilsmrdmybrckasuvrxzeszyrceeecuxmfsbxxnvyhalhuhcnedrzcsqfwulyqlnwdovcbsaksnrmvwczztyelqljnwkdhxzmlwooprzheqwzoyzotzuhsfqfbailzkzkgobxfaflsdlhdmxylrhdwykzavkchkmpannhpuskxdrcrocfhkkdkvemqrozddodsbmivesuklybbbgintnxorjgiafldvqxxcfujzptmnwnqfxdymcoemwoobcrwuovxwqgxfbmuzznezselvsrhoivrmoqgeldcdlakomfonuuomzahttqkdndpdvwihzkknmxbaeroymcmneuiamudaytftpncqapdmmhnqadomhcbmwkmlgrneksazicqitvwfelxfontyqvkohyznrmuchubpytxsrvzvhxaxmuvcrhryhsnzfhbmdedcacwrxxmeupejjzlcjeaqxprexssazvezxaquyebuthdegrgrviisybwxuozvleuiemfeqizmpeqxvzprjzljakguagrmmlwszdxqlyeacuyavnrfmeomqxjrfcfrqbjpfihsgltlitldiekwibkzavsfyxyoajdhsnaayvliqyoycyunbgobcggcfgqhqvpkirkjmytcwhcbiyojeelimtkracysypcigotuopynwaoeyeqfibnuwynwcnqcdsnljggxxemuhaafgskovaksyzqcnhfsbsdkalkyswuzzugfnrwiricritvvaandssoveatwynxxjdfywbjpllitgeoiedmzwcwccpyaexapafqzizotmxntddbjhnifrghtxhwdewtgblfjkwcyvrravvjeiujaasvyzrbfnwlqhtbrrgqoxjouexyprjrsdmscgtjvqxoxsprjvjzvunpzkqqebhyfvqjdjtzbspbkeuvcduxxhoytgoxkvvptnawxvxanmbzrvuvaouavkyprpobkmsfzucyqzzfoafhyzcaoyalizczorcafuiepkdhlcmberietmfhkhutgxuwqxzvthbktergieipxqvqfazpiuiqrdphcqevzfgsmzaudildbhtgjvvtpgmglbpptvksasmosmlxvwawfweprbkghwouzfjsawmcqyrilcggaieggkxzlhhvgvrfcbaczmhpnayyeggeqmkzwjyjrgezdwegzgkytrpgtpgxxmcoaopwynhrtmqzkwfjdrjylageqocncgdcusaughnbtfcfchqhbbhmcpssduhfaljxdjtlqhpmmrrqbkzcliqnxihcfpjhcsehdatwhkivbboennizwyovjwyonhvpccyfctbldlghpulckwdgusbfobtdlruntsuvmyiplowalkfvgibcaeiqgdvrupgnickwasnmjkouwvpegsgiwqbeuwxoofeyiquijhwutnorkfvqxezvuaaoyzfmfjknzgxkowgjlubvzwoycnuxpwnnvsccdorfvprlnyuyuvvuujswyovpkjdidhkjveoqvfbifyiatfuproopflfpukzarmjmqxfleivkgdkckgrbwhtbtytpkspdeyruxwhefcqlhcnsihvgookjodzyxodcpvyorisykimukboukjdbankosjppastpnufhcyhoivivxwphisiihvwzenregkvvebcgbmxlhfggewomqywatsbagsgodcjjfosiaptjwcqaagoseksafuanoibrmmdtavbcmxmizcjzizrokmqrgseubhontrhwokyahmiiqthyvbpfnfwvycrzjhmeyvaogboppzyguhgpkvwxbuuzwmahtomukoksrbhhafnwptozpkzshttdhtndhzztpjlhkvomadlrwzrnlsfsnayxtladmbybkmixhyjjbwbavunjmvbzvbwrmywyzwcmikjplszmrammwcvizzjwagpngnngkpsscbinvjbmjqbfsyptxyraghpnueveupxyshlpqoebehiyfxtfxlfeswtoaqtwhsuzzhqrwuscbabjxuqwfxaztvotnjqubmtcuktsgnuknxvmulkbilbbirwwaoqdbmhxycrgpnqellshycigpjjycgwcumhxaowysnfhbxnolhvjkdqbpzbthxycmfbmwynonsyqpleatbmfhcgydkrlhpnvxqktoxjerhugvzjqcvxjzfhcpbnzqcupteijdunudkdyjmqnjnxkvgmkhcjaegpzmalquakxknyvbojmeiwzollynvytdpvrpghlgulthjolzxxdfwewhquwnupslmjbsroblzuuikhkkfnrbunkmjgzfxrrkcuctifgibdqmkcwrsnumdeyokzukqlushyopgwxhzqtmsrdkhyykewyrvubnhxiovwebudjwveioynafnzrsxrltmmitjqkgooiltthpzrdzefeuexocpzxbnhwyageowozqjihjzmigrpaymqcdpzgcidailgxkvyaejjpivaqmpskdimhznnbwsdegmdqheqnnzfxwfqiwivbthksxawvsbbynhowlgmahrzdypouajvtsilpihkajobwvrviyhihyjnwbbsbmpuexhufpgiaujyratzachkctzauasvmkrsjaoprxzrrislklknqszsnythaneoxxghxvypsxilhawtohiebhyqhrxhhdvgaepzbnqvmzgtbfppoxvrfuhongwujtblxqmygdtsecofjdxxasqhoxwcfuyqnbbnuplashuyjiesnbrqvhzkgxmlrmbhoeuithiooeoznstcvmsttsdbrrojdrsrvmbrtlhkqvlfqfhnrcgnulqlcfwiwobucdnyowketvjlctzuvhjrtzexedfdrhzwkmuakghdbsrelsaibjzukeuphzuaityuhuvtaeyewpiihkqjtsfdybufbqhlliiucdndjmhfdrpnbgwwadzyfwfimovawduziierwlxzemmfukbuocsvyzfnoankejjmrnepadpepdjkomgepbzpageogtzkdizwydmyiddxgojubqystzhtfmiofhykrzhpevomytkehvbknbcgdmakyiyfrzsiybadwqpwjzcsxdfkhhrkvtuvbtdssvcqwcvuslqdmsyjydufmnrigldjggdadftzkotezlimeixeltjxgahonlxwtcwxkpyuguaffktlsrpdmydtakjqxjmtliqxaqrgkunqejbxctcorhjustkrewkpgcngtgodcevhpzgrskhlsrfmmrdqpqhwswelfvrhzoqonyhhjotbhaptusljivfbrmoyhhigwywlxdtirojzoyuisiuddbgwnuxwefccdapcdkkxyvyptszoctrfvxektbhzilblororbyqcworpjqflmwtjvztpacqkqsvellvommqiajzlafxodqxbpqsrviblaypnkmeyyugbehmgolwsbtvslnjgvkrylinvvrvvmbwsilazwwraroscbejyqcwblkygdtbrualfnleljxlclgfokqmaslefxclawpssrdzerdrccffplkdxaszuddaainaeeswvywbpbvtgayvnkbkmtlhwrjyzyyddtbatzcidxbjzcgmmjyzejicarabpbeuuivphxxkwavvcejrnhqusawvrxwiyddulyjuxifvkcspgbgwkyqeamzrlyblbghogsifapansscavkwfkokkdldojgcmnjavclbeabrswvvrhtzrildlwgbindwcpzxrkyvkcpfykarutslbdpocxskkxgomfmfrmbcoshmehgyaymecbueamqlmybygnexahzobidfplgsjsynvstvhlitkktszzpzirqjbvptgxlrplhkfahmoqhaadszcjblctweonxwkkpzxwlcztrnjynldhmiqnvvgrzwfyurkkthgejhzawkbjxeskolxjggbheihzzrjsarzjbymxlpjivbskostuaudeckfedowdivplwbihqtpnxixlcricllgqymubcsfmowxkzirrbieaqcmstuwgffwdrshvlokbkjnjtgaxogxhijbbfyildgqwbzrondpznckiimfytdfbmhfsfpfkvxgyxzmmddosfyaplgqelvhzemtjbatzybxxbmlgpzyxyaoliertcytgnosoplppppmqbzwszwpzygmhkdcmdxtqmlhbywqpnefqddjebadagpjtulhyjaovnxyowodjulaxolcsnhsuzbptowtxziuohscdiinctpcjagbnnvjoyaknxdkynfmvzryiznmscodewfumafazgmodsydhfpcfgdpfsdzxudbqkvcmbdnjrbmjarrhgvonafut"s,
+			vector<string>{
+			"vkyprpobkmsfzucyqzzfoafhyzca", "gdvrupgnickwasnmjkouwvpegsgi", "jzukeuphzuaityuhuvtaeyewpiih", "syzqcnhfsbsdkalkyswuzzugfnrw", "kygdtbrualfnleljxlclgfokqmas", "pqoqebuckqhuuhtbqdfgmghlmwuq", "ycrgpnqellshycigpjjycgwcumhx", "vgogruifovqohxcdrjwroyazyfcm", "lefxclawpssrdzerdrccffplkdxa", "vyqhgvsvnjeciuhfazheupabuovy", "fexbifznmmmgjkpxujkgpzrfyjfj", "qthyvbpfnfwvycrzjhmeyvaogbop", "rvvmbwsilazwwraroscbejyqcwbl", "jzlafxodqxbpqsrviblaypnkmeyy", "vrhzoqonyhhjotbhaptusljivfbr", "trfvxektbhzilblororbyqcworpj", "sybwxuozvleuiemfeqizmpeqxvzp", "bxgdqeqawpblyyukmlarvzcitelf", "tjxgahonlxwtcwxkpyuguaffktls", "ebhyfvqjdjtzbspbkeuvcduxxhoy", "rplhkfahmoqhaadszcjblctweonx", "obxfaflsdlhdmxylrhdwykzavkch", "hykrzhpevomytkehvbknbcgdmaky", "tuvbtdssvcqwcvuslqdmsyjydufm", "bkmtlhwrjyzyyddtbatzcidxbjzc", "nrigldjggdadftzkotezlimeixel", "tjqkgooiltthpzrdzefeuexocpzx", "astpnufhcyhoivivxwphisiihvwz", "plashuyjiesnbrqvhzkgxmlrmbho", "ieaqcmstuwgffwdrshvlokbkjnjt", "dewtgblfjkwcyvrravvjeiujaasv", "hzeuvdirbumdcyhxvizqaspygypr", "bbupkrinqdgjgvrlhdcsznylcynb", "mwzmuyjvbijetxzatsszvmjrjqvw", "lsxqakjrgasvjyjlowpcftohhuww", "datwhkivbboennizwyovjwyonhvp", "stuaudeckfedowdivplwbihqtpnx", "chkctzauasvmkrsjaoprxzrrislk", "sypcigotuopynwaoeyeqfibnuwyn", "izwydmyiddxgojubqystzhtfmiof", "rsrvmbrtlhkqvlfqfhnrcgnulqlc", "tmfhkhutgxuwqxzvthbktergieip", "kmpannhpuskxdrcrocfhkkdkvemq", "pvyorisykimukboukjdbankosjpp", "fahqbehiccxdwctcrrvuekrbkuok", "nuuomzahttqkdndpdvwihzkknmxb", "agpngnngkpsscbinvjbmjqbfsypt", "zcbadxppoaeedlplihwqmzkoojbq", "bgwnuxwefccdapcdkkxyvyptszoc", "xzeszyrceeecuxmfsbxxnvyhalhu", "evhpzgrskhlsrfmmrdqpqhwswelf", "euithiooeoznstcvmsttsdbrrojd", "ibdqmkcwrsnumdeyokzukqlushyo", "babjxuqwfxaztvotnjqubmtcukts", "kqjtsfdybufbqhlliiucdndjmhfd", "csynisxanjxhkmrqfhylnszckigb", "dtsecofjdxxasqhoxwcfuyqnbbnu", "owkqffjiuctbhvweiybxmoxlisvv", "kbxpfwtpbscqgpwrbnovwadlauty", "nepadpepdjkomgepbzpageogtzkd", "mmhnqadomhcbmwkmlgrneksazicq", "rjgiafldvqxxcfujzptmnwnqfxdy", "tgoxkvvptnawxvxanmbzrvuvaoua", "oyalizczorcafuiepkdhlcmberie", "wcnqcdsnljggxxemuhaafgskovak", "tfcfchqhbbhmcpssduhfaljxdjtl", "gdryqmitkpmzlrrkatuvmyaaqosd", "zmhpnayyeggeqmkzwjyjrgezdweg", "qflmwtjvztpacqkqsvellvommqia", "rbhhafnwptozpkzshttdhtndhzzt", "kqmxzeqirbvclxyhbxcuxuodlfil", "epnbyezmlcalkfnkovaeydkixqsa", "crczptixoatdlziutpbtqjzlaamu", "avclbeabrswvvrhtzrildlwgbind", "jbxctcorhjustkrewkpgcngtgodc", "xwhefcqlhcnsihvgookjodzyxodc", "vgmkhcjaegpzmalquakxknyvbojm", "pjlhkvomadlrwzrnlsfsnayxtlad", "rzheqwzoyzotzuhsfqfbailzkzkg", "pbnzqcupteijdunudkdyjmqnjnxk", "zxxdfwewhquwnupslmjbsroblzuu", "zselvsrhoivrmoqgeldcdlakomfo", "qjamzfvbuamftloiiyvbncrekvol", "xyowodjulaxolcsnhsuzbptowtxz", "puuaisjoluoiyzlnkjeulwlpliuj", "dpzgcidailgxkvyaejjpivaqmpsk", "mfbmwynonsyqpleatbmfhcgydkrl", "qhpmmrrqbkzcliqnxihcfpjhcseh", "pflktfyvnllruiwigexwsnaydqcw", "mdedcacwrxxmeupejjzlcjeaqxpr", "bnhwyageowozqjihjzmigrpaymqc", "njsqglllijcwubytryptgrrfyype", "moyhhigwywlxdtirojzoyuisiudd", "gaxogxhijbbfyildgqwbzrondpzn", "qpryukxkwhfgzuiklzwnurkhpkjf", "rjzljakguagrmmlwszdxqlyeacuy", "eivkgdkckgrbwhtbtytpkspdeyru", "ikhkkfnrbunkmjgzfxrrkcuctifg", "sdmscgtjvqxoxsprjvjzvunpzkqq", "mywyzwcmikjplszmrammwcvizzjw", "ixlcricllgqymubcsfmowxkzirrb", "ahtrdntlhczsxncxonlyxrvkngab", "lruntsuvmyiplowalkfvgibcaeiq", "wqpnefqddjebadagpjtulhyjaovn", "eiwzollynvytdpvrpghlgulthjol", "tqfjzpfljszinvilqphopfejzpih", "gnuknxvmulkbilbbirwwaoqdbmhx", "ypouajvtsilpihkajobwvrviyhih", "smlxvwawfweprbkghwouzfjsawmc", "uvvuujswyovpkjdidhkjveoqvfbi", "vyswdflcyptilsmrdmybrckasuvr", "avnrfmeomqxjrfcfrqbjpfihsglt", "rpdmydtakjqxjmtliqxaqrgkunqe", "pmqbzwszwpzygmhkdcmdxtqmlhby", "vvcejrnhqusawvrxwiyddulyjuxi", "aounisgiepbkpthbtrmipbmuwcdg", "nlhxitreyrjwbxwyxritnacahfcb", "wivbthksxawvsbbynhowlgmahrzd", "zxyptkiyprijwcndxjjhhmgvbeij", "zgkytrpgtpgxxmcoaopwynhrtmqz", "zgtbfppoxvrfuhongwujtblxqmyg", "kvkuvdxdykoyvyuiwwqgmgdasydj", "jouksvkvubmidtoiegtonqgbrrrf", "yjnwbbsbmpuexhufpgiaujyratza", "zflnytcpjwzegkyfwfejgnivzadk", "eksafuanoibrmmdtavbcmxmizcjz", "qyrilcggaieggkxzlhhvgvrfcbac", "ayvliqyoycyunbgobcggcfgqhqvp", "wqbeuwxoofeyiquijhwutnorkfvq", "lgkjxzaffftnjlfamvoucfurecpj", "yxzxzbwoalmtgvsqedsiuwhdlrcg", "pdcblomjqnovukidvpgikwepebgi", "lwaqmptmwsajwaxifktvitbgwlqv", "zlnewjtfhcaxghjeqhzzzepgebvx", "jowjfqjyveldplwricppcnwevsnk", "hcnedrzcsqfwulyqlnwdovcbsaks", "kwfjdrjylageqocncgdcusaughnb", "mlgpzyxyaoliertcytgnosoplppp", "nvstvhlitkktszzpzirqjbvptgxl", "ccyfctbldlghpulckwdgusbfobtd", "faqqsepulfecjzgwzfdaehubbkeo", "dgksrupzpjfoaomdnjekamepngpw", "dosfyaplgqelvhzemtjbatzybxxb", "bbirsbqnlpzkyfsxbcimeebyxmhm", "kirkjmytcwhcbiyojeelimtkracy", "yruzsctlglytaxfyeyyfcgtgmxfk", "fxtfxlfeswtoaqtwhsuzzhqrwusc", "xzczpxnxrthxkijbeqcxhphysvur", "itvwfelxfontyqvkohyznrmuchub", "fwiwobucdnyowketvjlctzuvhjrt", "awtohiebhyqhrxhhdvgaepzbnqvm", "mbybkmixhyjjbwbavunjmvbzvbwr", "ywbjpllitgeoiedmzwcwccpyaexa", "pgwxhzqtmsrdkhyykewyrvubnhxi", "iricritvvaandssoveatwynxxjdf", "zexedfdrhzwkmuakghdbsrelsaib", "ypppiefofbqsrzooihpymwgyfnmr", "lknqszsnythaneoxxghxvypsxilh", "rocxqunjycflkjqtkwpjbsjfbggd", "qmtibvhtpucqyjbisrbfosopmaxo", "nrmvwczztyelqljnwkdhxzmlwoop", "vosmaimocjxcoxgnooqdeqqeqtek", "exssazvezxaquyebuthdegrgrvii", "yzrbfnwlqhtbrrgqoxjouexyprjr", "hpnvxqktoxjerhugvzjqcvxjzfhc", "ifapansscavkwfkokkdldojgcmnj", "gmmjyzejicarabpbeuuivphxxkwa", "mtgnqosivkjtzmfxmzjznkqilvsg", "aeroymcmneuiamudaytftpncqapd", "ovwebudjwveioynafnzrsxrltmmi", "litldiekwibkzavsfyxyoajdhsna", "wkkpzxwlcztrnjynldhmiqnvvgrz", "szuddaainaeeswvywbpbvtgayvnk", "kkxgomfmfrmbcoshmehgyaymecbu", "dimhznnbwsdegmdqheqnnzfxwfqi", "xqvqfazpiuiqrdphcqevzfgsmzau", "eamqlmybygnexahzobidfplgsjsy", "pytxsrvzvhxaxmuvcrhryhsnzfhb", "wegvdhnxrvxjoiskmfnloysylzvm", "xezvuaaoyzfmfjknzgxkowgjlubv", "pafqzizotmxntddbjhnifrghtxhw", "lxzemmfukbuocsvyzfnoankejjmr", "rpnbgwwadzyfwfimovawduziierw", "rozddodsbmivesuklybbbgintnxo", "izrokmqrgseubhontrhwokyahmii", "ojvtaxsrsdpidkmplulejmzcxznz", "diwphyhtochmwhvujsdofymptnvp", "dildbhtgjvvtpgmglbpptvksasmo", "rmoyluuhyfabzpgbnpykyadasuya", "ojktkrlnxvwzxtfuvayosuiygzzs", "ugbehmgolwsbtvslnjgvkrylinvv", "mcoemwoobcrwuovxwqgxfbmuzzne", "aowysnfhbxnolhvjkdqbpzbthxyc", "mgejpsrgnzodvercdtfuedoueotc", "fyiatfuproopflfpukzarmjmqxfl", "zwoycnuxpwnnvsccdorfvprlnyuy", "ckiimfytdfbmhfsfpfkvxgyxzmmd", "kytbhtnjakkulwagkqquomvbomhy", "dxttdxwomkzczqxuaizxxhfkembi", "fvkcspgbgwkyqeamzrlyblbghogs", "wcpzxrkyvkcpfykarutslbdpocxs", "enregkvvebcgbmxlhfggewomqywa", "iyfrzsiybadwqpwjzcsxdfkhhrkv", "wfyurkkthgejhzawkbjxeskolxjg", "xyraghpnueveupxyshlpqoebehiy", "gbheihzzrjsarzjbymxlpjivbsko", "pzyguhgpkvwxbuuzwmahtomukoks", "tsbagsgodcjjfosiaptjwcqaagos", "ljgaotljyjgnwshsafaijzfdmfcs"
+		});
+	};
+}
+
+int main(int argc, char* argv[]) {
+	int result = Catch::Session().run(argc, argv);
+	return result;
+}
